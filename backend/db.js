c3f592d31ba6bb0533c51f69abf150db821ddcae
juanmaherrera94 <101391003+juanmaherrera94@users.noreply.github.com>
2025-06-04 10:35:34 +0200

    commit

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,21 @@
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME || "gestion_tenis",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  // Producción en Railway (usa la URL completa)
+  pool = mysql.createPool(process.env.DATABASE_URL);
+} else {
+  // Desarrollo local (usa host, usuario, etc.)
+  pool = mysql.createPool({
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || "",
+    database: process.env.DB_NAME || "gestion_tenis",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+}
 
 module.exports = pool;
