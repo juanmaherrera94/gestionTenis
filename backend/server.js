0367f5760ef1fb7e8e600216b86dfbd9a5f20ca1
juanmaherrera94 <101391003+juanmaherrera94@users.noreply.github.com>
2025-06-04 11:14:25 +0200

    commit2222

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,7 @@ const port = process.env.PORT || 4000
 
 // Configuración de CORS para permitir solicitudes desde localhost:5173 (tu frontend Vue)
 const corsOptions = {
-  origin: ["http://localhost:5173","https://gestiontenis-production.up.railway.app/"]
-  ,
+  origin: ["http://localhost:5173","https://gestiontenis-production.up.railway.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"] // Permite el encabezado Content-Type
 };
