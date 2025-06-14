230bbb63bf41468f53b139043eac133ae94653e7
juanmaherrera94 <101391003+juanmaherrera94@users.noreply.github.com>
2025-06-14 15:34:39 +0200

    cc

--- backend/db.js
+++ backend/db.js
@@ -2,23 +2,6 @@ const mysql = require("mysql2/promise");
 
 let pool;
 
-/* if (process.env.DATABASE_URL) {/* 
- */  // Parsear la URL usando la clase URL para extraer los datos
-  /* const { URL } = require("url");
-  const dbUrl = new URL(process.env.DATABASE_URL);
-
-  pool = mysql.createPool({
-    host: dbUrl.hostname,
-    port: dbUrl.port,
-    user: dbUrl.username,
-    password: dbUrl.password,
-    database: dbUrl.pathname.substring(1),
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-  }) */
-/* } else { */
-  // Configuración para desarrollo local
   pool = mysql.createPool({
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
@@ -28,6 +11,6 @@ let pool;
     connectionLimit: 10,
     queueLimit: 0,
   });
-/* } */
+
 
 module.exports = pool;
